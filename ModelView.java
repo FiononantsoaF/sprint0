e2f23099ad362374de0a0f59c340c31289bcf44a
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-06-05 21:17:57 +0200

    sprint4

--- ModelView.java
+++ ModelView.java
@@ -12,31 +12,31 @@ import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
-        
+
 public class ModelView {
-    String url;
-    Map<String, Object> data = new HashMap<>();
+    private String url;
+    private Map<String, Object> data = new HashMap<>();
 
     public ModelView(String url) {
-        this.url(url);
+        this.url = url;
     }
-        
+
     public Map<String, Object> getData() {
         return data;
     }
-        
-    public void setData(HashMap<String, Object> data) {
+
+    public void setData(Map<String, Object> data) {
         this.data = data;
     }
-        
+
     public String getUrl() {
         return url;
     }
-        
+
     public void setUrl(String url) {
         this.url = url;
     }
-        
+
     public ModelView addObject(String key, Object value) {
         this.data.put(key, value);
         return this;
