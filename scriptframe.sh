c2d652dfb0efb426691b8b7c04ea52e44bfce403
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-06-05 14:47:35 +0200

    sprint4

--- scriptframe.sh
+++ scriptframe.sh
@@ -8,9 +8,11 @@ javac -d . *.java
 jar cf FrontController.jar  mg
 jar cf AnnotationController.jar  mg
 jar cf GetAnnotation.jar  mg
+jar cf ModelView.jar  mg
 
 mv FrontController.jar "G:/S4/MrNaina/work/test/lib"
 mv AnnotationController.jar "G:/S4/MrNaina/work/test/lib"
 mv GetAnnotation.jar "G:/S4/MrNaina/work/test/lib"
+mv ModelView.jar "G:/S4/MrNaina/work/test/lib"
 sleep 60
 
