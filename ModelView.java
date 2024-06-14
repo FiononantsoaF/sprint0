9ca592d99fe7f9a465a37eac178ed5deca09d109
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-06-14 10:25:58 +0200

    sprint6

--- ModelView.java
+++ ModelView.java
@@ -1,47 +1,27 @@
 package mg.itu.prom16;
 
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.Map;
 
 public class ModelView {
     private String url;
-    private Map<String, Object> data = new HashMap<>();
+    private HashMap<String, Object> data;
 
-    public ModelView(){
-        
-    }
     public ModelView(String url) {
         this.url = url;
+        this.data = new HashMap<>();
     }
 
-    public Map<String, Object> getData() {
-        return data;
-    }
-
-    public void setData(Map<String, Object> data) {
-        this.data = data;
+    public void addObject(String name, Object value) {
+        data.put(name, value);
     }
 
     public String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public ModelView addObject(String key, Object value) {
-        this.data.put(key, value);
-        return this;
+    public HashMap<String, Object> getData() {
+        return data;
     }
 }
+
+
