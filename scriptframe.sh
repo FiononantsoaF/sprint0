b957f54af98e5ed8786a8d4db9be94843c4928c2
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-06-14 10:26:58 +0200

    sprint6

--- scriptframe.sh
+++ scriptframe.sh
@@ -9,10 +9,14 @@ jar cf FrontController.jar  mg
 jar cf AnnotationController.jar  mg
 jar cf GetAnnotation.jar  mg
 jar cf ModelView.jar  mg
+jar cf Post.jar  mg
+jar cf Param.jar  mg
 
-mv FrontController.jar "G:/S4/MrNaina/work/test/lib"
-mv AnnotationController.jar "G:/S4/MrNaina/work/test/lib"
-mv GetAnnotation.jar "G:/S4/MrNaina/work/test/lib"
-mv ModelView.jar "G:/S4/MrNaina/work/test/lib"
+mv FrontController.jar "G:\S4\MrNaina\work\testFramework\test\lib"
+mv AnnotationController.jar "G:\S4\MrNaina\work\testFramework\test\lib"
+mv GetAnnotation.jar "G:\S4\MrNaina\work\testFramework\test\lib"
+mv ModelView.jar "G:\S4\MrNaina\work\testFramework\test\lib"
+mv Post.jar "G:\S4\MrNaina\work\testFramework\test\lib"
+mv Param.jar "G:\S4\MrNaina\work\testFramework\test\lib"
 sleep 60
 
