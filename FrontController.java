a293a68f62c00cb05332c518c3ee84847adebbf7
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-10-20 19:45:32 +0200

    sprint11

--- FrontController.java
+++ FrontController.java
@@ -244,14 +244,7 @@ class Mapping {
         this.verbActions.add(verbAction);
     }
 
-    public VerbAction getVerbActionByVerb(String verb) {
-        for (VerbAction action : verbActions) {
-            if (action.getVerb().equalsIgnoreCase(verb)) {
-                return action;
-            }
-        }
-        return null;  
-    }
+
 
     @Override
     public String toString() {
