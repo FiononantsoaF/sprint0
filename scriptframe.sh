a8b902a6608ea7a8b2b2344cc3bd4e22d3c3e351
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2025-02-18 09:06:58 +0100

    sprint14

--- scriptframe.sh
+++ scriptframe.sh
@@ -6,7 +6,6 @@ test_lib_dir="G:/S4/MrNaina/work/testSprint13/lib"
 
 cd "$frame_dir" || exit
 
-# Compile Java files with necessary libraries in the classpath
 javac -d "$frame_dir" -cp "$lib_dir/*" "$frame_dir"/*.java
 for jar_file in FrontController AnnotationController GetAnnotation StringType IntType DoubleType  ModelView Post NotNull Param ParamField ParamObject InjectionSession Url  AnnotationClass RestApi AnnotationAttribut CustomSession VerbAction; do
     jar cf "${jar_file}.jar" mg
